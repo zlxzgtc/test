19f8c622547fe1dfd9698882149da8f3f3382b90
zlx <lexiaozhang@foxmail.com>
2024-04-24 11:42:08 +0800

    init

--- cicd/src/main/java/org/example/Main.java
+++ cicd/src/main/java/org/example/Main.java
@@ -11,7 +11,8 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
             // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
+            System.out.println("i =  " + i);
+
         }
     }
 }
